e1aa4d7226b3302448a70a9e9657ba8f9ce70e39
Piotr Patrzyk <piotr@patrzyk.me>
2024-05-03 14:48:47 +0200

    snake growth ok

--- src/objects/Snake.ts
+++ src/objects/Snake.ts
@@ -1,20 +1,18 @@
 import { GameObjects, Scene } from "phaser";
-import { Coordinates, STEP_SIZE } from "../scenes/Game"
+import { Coordinate, CoordinateList, Game, STEP_SIZE } from "../scenes/Game"
 
 type Direction = "up" | "down" | "right" | "left"
+type Body = Array<GameObjects.Image>;
 
 export class Snake extends GameObjects.GameObject {
     
-    snakeBody: GameObjects.Group;
+    snakeBody: Body = new Array();
     currentDirection: Direction = "right";
     stepSize: number = STEP_SIZE;
 
-    constructor(scene: Scene) {
+    constructor(scene: Scene, initX: number, initY: number) {
         super(scene, "Snake");
-        this.snakeBody = new GameObjects.Group(scene);
-        let bodyPart: GameObjects.Image = new GameObjects.Image(this.scene, this.stepSize, this.stepSize, "snake_body");
-        bodyPart.setOrigin(0)
-        this.snakeBody.add(bodyPart, true);
+        this.addBodyPart(initX, initY);
     }
 
     // https://stackoverflow.com/a/44827922
@@ -23,54 +21,58 @@ export class Snake extends GameObjects.GameObject {
     };
 
     changeDirection(newDirection: Direction) {
+        // TODO fix reversing direction
+        // must keep last effective direction, possible to revrt with multiple actions between tick
         let horizontal: Set<Direction> = new Set(["left", "right"]);
         let vertical: Set<Direction> = new Set(["down", "up"]);
         let change: Set<Direction> = new Set([this.currentDirection, newDirection]);
         if (this.areSetsEqual(change, horizontal) || this.areSetsEqual(change, vertical)) {
             // ignore, opposite direction
         } else {
-            console.log("changing")
             this.currentDirection = newDirection;
         }
     }
 
-    public getBodyCoordinates(): Coordinates {
-        // TODO fix: foreach treat as image
-        this.snakeBody.getChildren().forEach(bodyPart: GameObjects.Image => {[bodyPart.x, bodyPart.y]})
+    public getBodyCoordinates(): CoordinateList {
+        return this.snakeBody.map(child => {
+            let game = this.scene as Game
+            return game.extractXY(child)
+        })
     }
 
-    grow():void {
-        let head = this.snakeBody.getFirst(true);
-        let tail = this.snakeBody.getLast(true);
-        console.log(head)
-        console.log(tail)
-        let x:number = 0;
-        let y:number = 0;
+    addBodyPart(x:number, y:number):void {
         let bodyPart: GameObjects.Image = new GameObjects.Image(this.scene, x, y, "snake_body");
         bodyPart.setOrigin(0)
-        this.snakeBody.add(bodyPart, true);
+        this.scene.add.existing(bodyPart);
+        this.snakeBody.push(bodyPart);
     }
 
-    move(): void {
+    move(grow:boolean): void {
+        let head = this.snakeBody[this.snakeBody.length-1] as GameObjects.Image;
+        let tail = this.snakeBody[0] as GameObjects.Image;
         let x: number
         let y: number
         switch (this.currentDirection) {
             case "left":
-                [x, y] = [-this.stepSize,  0]
+                [x, y] = [head.x-this.stepSize, head.y]
                 break
             case "right":
-                [x, y] = [this.stepSize,  0]
+                [x, y] = [head.x+this.stepSize, head.y]
                 break
             case "up":
-                [x, y] = [0, -this.stepSize]
+                [x, y] = [head.x, head.y-this.stepSize]
                 break
             case "down":
-                [x, y] = [0, this.stepSize]
+                [x, y] = [head.x, head.y+this.stepSize]
                 break
             default:
-                [x, y] = [0, 0];
+                [x, y] = [head.x, head.y];
                 break
         }
-        this.snakeBody.incXY(x, y);
+        if (!grow) {
+            this.snakeBody.shift();
+            tail.destroy(true);
+        }
+        this.addBodyPart(x, y);
     }
 }
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,7 +2,8 @@ import { GameObjects, Scene } from 'phaser';
 import { Snake } from "../objects/Snake"
 import { Apple } from '../objects/Apple';
 
-export type Coordinates = Array<Array<number>>;
+export type Coordinate = `${number}_${number}`;
+export type CoordinateList = Array<Coordinate>;
 export const STEP_SIZE: number = 32;
 
 export class Game extends Scene
@@ -10,37 +11,44 @@ export class Game extends Scene
     camera: Phaser.Cameras.Scene2D.Camera;
     snake: Snake;
     apple: Apple;
-    gameCoordinates: Coordinates;
+    gameCoordinates: CoordinateList;
     
-    stepFreq: number = 500;
-    timer: number = 500;
+    stepSize: number = STEP_SIZE;
+    stepFreq: number = 100;
+    timer: number = 100;
 
     constructor() {
         super('Game');
     }
 
-    // possible coordicates within game, not taken by snake
-    private getEmptyCoordinates() {
-        // TODO all - snake
-        // this.snake.getBodyCoordinates()
-        return this.gameCoordinates
+    extractXY(object: GameObjects.GameObject): Coordinate {
+        let bodyPart = object as GameObjects.Image
+        return `${bodyPart.x}_${bodyPart.y}`
+    }
+
+    // possible coordinates within game, not taken by snake
+    private getEmptyCoordinates(snakeCoordinates: CoordinateList): CoordinateList {
+        let empty = this.gameCoordinates.filter(x => !snakeCoordinates.includes(x));
+        return empty
     }
 
     create(): void {
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor("#A0A0A0");
 
-        this.snake = new Snake(this)
+        this.snake = new Snake(this, this.stepSize, this.stepSize)
         this.snake = this.add.existing(this.snake)
+
+        this.apple = new Apple(this, this.stepSize*4, this.stepSize*4);
+        this.apple.setOrigin(0)
+        this.apple = this.add.existing(this.apple);
 
         this.gameCoordinates = new Array();
         for (let i = 0; i < Number(this.game.config.width); i += STEP_SIZE) {
             for (let j = 0; j < Number(this.game.config.height); j += STEP_SIZE) {
-                this.gameCoordinates.push([i, j])
+                this.gameCoordinates.push(`${i}_${j}`)
             }
         }
-
-        // this.apple = new Apple(this, )
 
         this.input.keyboard!.on('keydown-A', () => {this.snake.changeDirection("left")});
         this.input.keyboard!.on('keydown-LEFT', () => {this.snake.changeDirection("left")});
@@ -53,12 +61,28 @@ export class Game extends Scene
     }
 
     tick() {
-        console.log("update")
-        console.log(this.getEmptyCoordinates())
-        console.log(this.snake.getBodyCoordinates())
-        // if eats, grow, otherwise move
-        // this.snake.grow();
-        this.snake.move();
+        let snakeCoordinates = this.snake.getBodyCoordinates()
+        let score: number = snakeCoordinates.length;
+        console.log("tick, score ", score)
+        let appleCoordinate = this.extractXY(this.apple);
+        if (snakeCoordinates.includes(appleCoordinate)) {
+            this.snake.move(true);
+            this.apple.destroy(true);
+            let emptyCoordinates = this.getEmptyCoordinates(snakeCoordinates);
+            // todo if empty, game over
+            let randomCoordinate: Coordinate = emptyCoordinates[Math.floor(Math.random() * emptyCoordinates.length)];
+            let [x, y] = randomCoordinate.split("_").map(c => Number(c));
+            this.apple = new Apple(this, x, y);
+            this.apple.setOrigin(0)
+            this.apple = this.add.existing(this.apple);
+        // } else if () {
+            // TODO die on collision
+        //     // snake collision, game over
+        } else {
+            this.snake.move(false);
+        }
+        // check collision here
+
         // check collision 
         // this.game.config.width
         // this.scene.sta rt('GameOver');
